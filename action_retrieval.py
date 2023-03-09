efc6819e0f9b6aebc973941d499b17286d9b5ad9
Misterfun88 <misterfu7@proton.me>
2023-03-09 19:56:17 +0000

    Changed a use of NoSpecimen to the ctor w/out req.

--- action_retrieval.py
+++ action_retrieval.py
@@ -12,3 +12,33 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
+import torch.utils.data
+from torch.utils.data import Dataset, DataLoader
+import torch.utils.data.distributed
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+
+from umurl import Downstream
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# change for action recogniton
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 32)')
+parser.add_argument('--epochs', default=80, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=30., type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--schedule', default=[50, 70, ], nargs='*', type=int,
+                    help='learning rate schedule (when to drop lr by a ratio)')
