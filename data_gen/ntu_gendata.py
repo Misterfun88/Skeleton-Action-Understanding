9e4415c702671ea42a326cea600e55e41ea09390
Misterfun88 <misterfu7@proton.me>
2023-04-01 09:00:18 +0000

    Back to development.

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -9,4 +9,5 @@ from data_gen.preprocess import pre_normalization
 
 # ntu 60
 training_subjects = [
-    1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19,
+    1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38
+]
