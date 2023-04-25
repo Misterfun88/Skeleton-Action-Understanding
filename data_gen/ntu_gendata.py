68257dff36b3e7bf35a2dc6b29a2c8455bd60866
Misterfun88 <misterfu7@proton.me>
2023-04-25 05:24:49 +0000

    TTY: fix tty_wait_until_sent on 64-bit machines

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -20,4 +20,6 @@ training_subjects = [
 ]
 training_setups = [ 2, 4,  6, 8, 10, 12, 14,  16,  18,  20, 22, 24, 26, 28, 30, 32]
 
-training_cameras = 
+training_cameras = [2, 3]
+max_body_true = 2
+max_body_
