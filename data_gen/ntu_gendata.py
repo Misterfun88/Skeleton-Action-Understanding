30d963f474ee57c5cd93cd14fdf0ce81dd8781f2
Misterfun88 <misterfu7@proton.me>
2023-05-06 22:01:40 +0000

    Auto-merged.

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -36,4 +36,4 @@ def read_skeleton_filter(file):
         skeleton_sequence['numFrame'] = int(f.readline())
         skeleton_sequence['frameInfo'] = []
         # num_body = 0
-        for t in 
+        for t in range(skeleton_sequence['numFra
