d330d2adcc449e585be886b6b690cfce7534ea20
Misterfun88 <misterfu7@proton.me>
2023-05-07 18:30:16 +0000

    Feature Development: Updated openHistorian tool: HistorianView.

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -36,4 +36,5 @@ def read_skeleton_filter(file):
         skeleton_sequence['numFrame'] = int(f.readline())
         skeleton_sequence['frameInfo'] = []
         # num_body = 0
-        for t in range(skeleton_sequence['numFra
+        for t in range(skeleton_sequence['numFrame']):
+            fram
