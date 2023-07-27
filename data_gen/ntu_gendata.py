531677f7c877e082d84a0e856e16ba3c08b98a62
Misterfun88 <misterfu7@proton.me>
2023-07-27 06:06:42 +0000

    update 3 files: biblio/biblio-ed.data, biblio/biblio-wg.data, wpt-tests.txt

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -102,4 +102,4 @@ def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
 
 
 def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', part='eval'):
-    Bone = [(1, 2), (2, 21), (3, 21), (4, 3),
+    Bone = [(1, 2), (2, 21), (3, 21), (4, 3), (5, 21), (6, 5), (7, 6), (8, 7), (
