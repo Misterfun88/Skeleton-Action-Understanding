feeac7ce42ae5f3c331aa5757b1e8b1a57b4c612
Misterfun88 <misterfu7@proton.me>
2023-10-16 17:45:11 +0000

    Update permissions.lua

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -178,4 +178,5 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
     
-    # parser = argparse.ArgumentParser(description='
+    # parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
+   
