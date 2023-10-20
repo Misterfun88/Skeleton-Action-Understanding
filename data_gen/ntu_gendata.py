9750c432576fb80643f646bce68ab50b0b9e2d0f
Misterfun88 <misterfu7@proton.me>
2023-10-20 15:16:04 +0000

    Added translation using Weblate (Slovak)

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -180,4 +180,5 @@ if __name__ == '__main__':
     
     # parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
     # parser.add_argument('--data_path', default='../data/nturgbd_raw/nturgb+d_skeletons/')
-    # parser.add_argument('--ignore
+    # parser.add_argument('--ignored_sample_path',
+    #               
