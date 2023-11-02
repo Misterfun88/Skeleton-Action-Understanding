115ebd9196dd2caa8d1a4a0d9201815eba8d6868
Misterfun88 <misterfu7@proton.me>
2023-11-02 11:10:47 +0000

    added a new test

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -197,4 +197,4 @@ if __name__ == '__main__':
     for b in benchmark:
         for p in part:
             out_path = os.path.join(arg.out_folder, b)
-      
+            if not os.path.exists(out_
