2a29ff77cb5fe63a808db19e90bff8c734703ea5
Misterfun88 <misterfu7@proton.me>
2023-11-20 15:14:19 +0000

    Update about-me.md

--- data_gen/preprocess.py
+++ data_gen/preprocess.py
@@ -25,3 +25,30 @@ def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
             for i_f, frame in enumerate(person):
                 if frame.sum() == 0:
                     if person[i_f:].sum() == 0:
+                        rest = len(person) - i_f
+                        num = int(np.ceil(rest / i_f))
+                        pad = np.concatenate([person[0:i_f] for _ in range(num)], 0)[:rest]
+                        s[i_s, i_p, i_f:] = pad
+                        break
+
+    print('sub the center joint #1 (spine joint in ntu and neck joint in kinetics)')
+    for i_s, skeleton in enumerate(tqdm(s)):
+        if skeleton.sum() == 0:
+            continue
+        main_body_center = skeleton[0][:, 1:2, :].copy()
+        for i_p, person in enumerate(skeleton):
+            if person.sum() == 0:
+                continue
+            mask = (person.sum(-1) != 0).reshape(T, V, 1)
+            s[i_s, i_p] = (s[i_s, i_p] - main_body_center) * mask
+
+    # print('parallel the bone between hip(jpt 0) and spine(jpt 1) of the first person to the z axis')
+    # for i_s, skeleton in enumerate(tqdm(s)):
+    #     if skeleton.sum() == 0:
+    #         continue
+    #     joint_bottom = skeleton[0, 0, zaxis[0]]
+    #     joint_top = skeleton[0, 0, zaxis[1]]
+    #     axis = np.cross(joint_top - joint_bottom, [0, 0, 1])
+    #     angle = angle_between(joint_top - joint_bottom, [0, 0, 1])
+    #     matrix_z = rotation_matrix(axis, angle)
+    #     for i_p, person in enumerate(skeleton):
