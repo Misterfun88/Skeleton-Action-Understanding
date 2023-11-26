094d42524577770125a7a680029c5dffa2174d8a
Misterfun88 <misterfu7@proton.me>
2023-11-26 12:22:30 +0000

    added authentication

--- data_gen/preprocess.py
+++ data_gen/preprocess.py
@@ -52,3 +52,37 @@ def pre_normalization(data, zaxis=[0, 1], xaxis=[8, 4]):
     #     angle = angle_between(joint_top - joint_bottom, [0, 0, 1])
     #     matrix_z = rotation_matrix(axis, angle)
     #     for i_p, person in enumerate(skeleton):
+    #         if person.sum() == 0:
+    #             continue
+    #         for i_f, frame in enumerate(person):
+    #             if frame.sum() == 0:
+    #                 continue
+    #             for i_j, joint in enumerate(frame):
+    #                 s[i_s, i_p, i_f, i_j] = np.dot(matrix_z, joint)
+
+    print(
+        'parallel the bone between right shoulder(jpt 8) and left shoulder(jpt 4) of the first person to the x axis')
+    for i_s, skeleton in enumerate(tqdm(s)):
+        if skeleton.sum() == 0:
+            continue
+        joint_rshoulder = skeleton[0, 0, xaxis[0]]
+        joint_lshoulder = skeleton[0, 0, xaxis[1]]
+        axis = np.cross(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+        angle = angle_between(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+        matrix_x = rotation_matrix(axis, angle)
+        for i_p, person in enumerate(skeleton):
+            if person.sum() == 0:
+                continue
+            for i_f, frame in enumerate(person):
+                if frame.sum() == 0:
+                    continue
+                for i_j, joint in enumerate(frame):
+                    s[i_s, i_p, i_f, i_j] = np.dot(matrix_x, joint)
+
+    data = np.transpose(s, [0, 4, 2, 3, 1])
+    return data
+
+
+if __name__ == '__main__':
+    data = np.load('../data/ntu/xview/val_data.npy')
+    pre_normalization(data)
