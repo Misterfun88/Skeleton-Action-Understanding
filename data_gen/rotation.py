3da201701282f58e0f0f39e65bd01ab46220e5ce
Misterfun88 <misterfu7@proton.me>
2023-12-05 21:59:46 +0000

    changed some text

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -7,3 +7,4 @@ def rotation_matrix(axis, theta):
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
     """
+    if np.abs(axis).sum() < 1e-6 or 
