a099bb1b49d0e22e5e56096369f59a3ac366904a
Misterfun88 <misterfu7@proton.me>
2023-12-06 08:22:55 +0000

    add crossrefs

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -7,4 +7,5 @@ def rotation_matrix(axis, theta):
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
     """
-    if np.abs(axis).sum() < 1e-6 or 
+    if np.abs(axis).sum() < 1e-6 or np.abs(theta) < 1e-6:
+        
