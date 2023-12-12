700dc6101355d718a363804916094355ee0925b6
Misterfun88 <misterfu7@proton.me>
2023-12-12 09:18:09 +0000

    some restructuring

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -16,4 +16,4 @@ def rotation_matrix(axis, theta):
     aa, bb, cc, dd = a * a, b * b, c * c, d * d
     bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
     return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
-                     [2 * (bc - ad), a
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd 
