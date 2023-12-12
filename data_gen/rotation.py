9bdf5e45fe11a4c6cf3ec41181d163e29ae5a4d7
Misterfun88 <misterfu7@proton.me>
2023-12-12 11:03:51 +0000

    Update rabbitmq

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -17,4 +17,4 @@ def rotation_matrix(axis, theta):
     bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
     return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
                      [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
-                     [2
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb 
