f1e46fa1ce036066bd07976a578bc9042d73dcf4
Misterfun88 <misterfu7@proton.me>
2023-12-16 09:14:51 +0000

    Testing git

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -29,4 +29,5 @@ def angle_between(v1, v2):
     """ Returns the angle in radians between vectors 'v1' and 'v2'::
 
             >>> angle_between((1, 0, 0), (0, 1, 0))
-            1.5
+            1.5707963267948966
+            >>> a
