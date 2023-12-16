3ef7c88f0db35c7bf2a2a4d062117dab04aaaa06
Misterfun88 <misterfu7@proton.me>
2023-12-16 12:39:26 +0000

    llc: fix info leak via getsockname()

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -30,4 +30,6 @@ def angle_between(v1, v2):
 
             >>> angle_between((1, 0, 0), (0, 1, 0))
             1.5707963267948966
-            >>> angle_between((1, 0, 0), (
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >
