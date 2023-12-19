368b076e7b078e22fe6a6728cbbdbb89f5cca0ef
Misterfun88 <misterfu7@proton.me>
2023-12-19 14:06:57 +0000

    v2.9.0

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -39,4 +39,7 @@ def angle_between(v1, v2):
         return 0
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def x_rotation(vect
