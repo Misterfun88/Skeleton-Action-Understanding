b3fca9096ffc9919381c76ba94148867a21a051e
Misterfun88 <misterfu7@proton.me>
2023-12-24 17:31:53 +0000

    CC-3174 : showbuilder

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -50,4 +50,4 @@ def x_rotation(vector, theta):
 
 def y_rotation(vector, theta):
     """Rotates 3-D vector around y-axis"""
-    R = np.array([[np.cos(theta), 0, np.sin
+    R = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(
