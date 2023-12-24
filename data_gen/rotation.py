d0dd7bdecc971e315507919383d4677d470ba2f4
Misterfun88 <misterfu7@proton.me>
2023-12-24 11:13:44 +0000

    Upgrading to Cedar-14

--- data_gen/rotation.py
+++ data_gen/rotation.py
@@ -44,4 +44,4 @@ def angle_between(v1, v2):
 
 def x_rotation(vector, theta):
     """Rotates 3-D vector around x-axis"""
-    R = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta
+    R = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(the
