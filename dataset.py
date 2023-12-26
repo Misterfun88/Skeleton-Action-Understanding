592e86c4283df6e045089aa1e8bba7f480e9ee27
Misterfun88 <misterfu7@proton.me>
2023-12-26 19:18:04 +0000

    added a gitignore file

--- dataset.py
+++ dataset.py
@@ -14,3 +14,10 @@ def get_finetune_training_set(opts):
     data = Feeder(**opts.train_feeder_args)
 
     return data
+
+def get_finetune_validation_set(opts):
+
+    from feeder.feeder_downstream import Feeder
+    data = Feeder(**opts.test_feeder_args)
+
+    return data
---
+++ distributed.py
@@ -0,0 +1,27 @@
+
+import torch
+import os
+import torch.distributed as dist
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
