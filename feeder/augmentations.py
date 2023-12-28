689d58f152af56557acc2a00b23cb8ce5f8506c8
Misterfun88 <misterfu7@proton.me>
2023-12-28 09:24:49 +0000

    - added documentation

--- feeder/augmentations.py
+++ feeder/augmentations.py
@@ -17,3 +17,39 @@ def joint_courruption(input_data):
         joint_indicies = np.random.choice(25, 15,replace=False)
         out[:,:,joint_indicies,:] = 0 
         return out
+    
+    else:
+         #joint_indicies = np.random.choice(25, random.randint(5, 10), replace=False)
+         joint_indicies = np.random.choice(25, 15,replace=False)
+         
+         temp = out[:,:,joint_indicies,:] 
+         Corruption = np.array([
+                           [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)],
+                           [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)],
+                           [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)] ])
+         temp = np.dot(temp.transpose([1, 2, 3, 0]), Corruption)
+         temp = temp.transpose(3, 0, 1, 2)
+         out[:,:,joint_indicies,:] = temp
+         return out
+
+
+
+def pose_augmentation(input_data):
+
+
+        Shear       = np.array([
+                      [1,	random.uniform(-1, 1), 	random.uniform(-1, 1)],
+                      [random.uniform(-1, 1), 1, 	random.uniform(-1, 1)],
+                      [random.uniform(-1, 1), 	random.uniform(-1, 1),      1]
+                      ])
+
+        temp_data = input_data.copy()
+        result =  np.dot(temp_data.transpose([1, 2, 3, 0]),Shear.transpose())
+        output = result.transpose(3, 0, 1, 2)
+
+        return output
+
+def temporal_cropresize(input_data,num_of_frames,l_ratio,output_size):
+
+
+    C, T, V, M =input_data.shape
