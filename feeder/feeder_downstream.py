b603f9ec2b42c299fc9dfdf1f6f201f8b0f871d0
Misterfun88 <misterfu7@proton.me>
2024-01-14 22:22:46 +0000

    Add more translations

--- feeder/feeder_downstream.py
+++ feeder/feeder_downstream.py
@@ -45,4 +45,4 @@ class Feeder(torch.utils.data.Dataset):
                      (18, 17), (19, 18), (20, 19), (21, 21), (22, 23), (23, 8), (24, 25), (25, 12)]
         
         
-       
+        print(self.data.shape,len(sel
