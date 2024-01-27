070928519d7dfc620e784a214d115e331705baf1
Misterfun88 <misterfu7@proton.me>
2024-01-27 13:01:03 +0000

    Initial file upload

--- feeder/feeder_downstream.py
+++ feeder/feeder_downstream.py
@@ -98,4 +98,4 @@ class Feeder(torch.utils.data.Dataset):
         for v1,v2 in self.Bone:
             bone[:,:,v1-1,:] = data_numpy[:,:,v1-1,:] - data_numpy[:,:,v2-1,:]
         bt = bone.transpose(1,3,2,0)
-        bt = bt.reshape(self.input_size,self.M*self.V
+        bt = bt.reshape(self.input_size,self.M*self.V*self.C).astype('float32'
