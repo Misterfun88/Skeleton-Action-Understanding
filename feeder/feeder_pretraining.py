1f8136d039d214ef60693443b75b061d59a31091
Misterfun88 <misterfu7@proton.me>
2024-02-02 21:09:34 +0000

    Remove moot `version` property from bower.json

--- feeder/feeder_pretraining.py
+++ feeder/feeder_pretraining.py
@@ -5,3 +5,39 @@ import torch
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 import random
+
+try:
+    from feeder import augmentations
+except:
+    import augmentations
+
+
+class Feeder(torch.utils.data.Dataset):
+    """ 
+    Arguments:
+        data_path: the path to '.npy' data, the shape of data should be (N, C, T, V, M)
+    """
+
+    def __init__(self,
+                 data_path,
+                 num_frame_path,
+                 l_ratio,
+                 input_size,
+                 mmap=True):
+
+        self.data_path = data_path
+        self.num_frame_path= num_frame_path
+        self.input_size=input_size
+        self.crop_resize =True
+        self.l_ratio = l_ratio
+
+
+        self.load_data(mmap)
+
+        self.N, self.C, self.T, self.V, self.M = self.data.shape
+        self.S = self.V
+        self.B = self.V
+        self.Bone = [(1, 2), (2, 21), (3, 21), (4, 3), (5, 21), (6, 5), (7, 6), (8, 7), (9, 21),
+                     (10, 9), (11, 10), (12, 11), (13, 1), (14, 13), (15, 14), (16, 15), (17, 1),
+                     (18, 17), (19, 18), (20, 19), (21, 21), (22, 23), (23, 8), (24, 25), (25, 12)]
+        
