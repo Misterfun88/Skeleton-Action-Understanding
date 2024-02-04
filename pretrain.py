125ce09006db241d3929e937870aa7ffb7767d8a
Misterfun88 <misterfu7@proton.me>
2024-02-04 16:19:51 +0000

    adding pages

--- pretrain.py
+++ pretrain.py
@@ -1,3 +1,22 @@
 
 import argparse
 import math
+import os
+import random
+import shutil
+import time
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+
+from torch.utils.tensorboard import SummaryWriter
+from dataset import get_pretraining_set
+
+
+import torch.distributed as dist
+from distributed import init_distributed_mode
