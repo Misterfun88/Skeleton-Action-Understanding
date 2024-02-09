27f27d1044d75ef426b6120b355125955a51ea8b
Misterfun88 <misterfu7@proton.me>
2024-02-09 19:55:50 +0000

    Pontoon: Update Norwegian Bokmål (nb-NO) localization of AMO Co-authored-by: Håvar Henriksen <havar@firefox.no>

--- script_action_recognition.sh
+++ script_action_recognition.sh
@@ -6,4 +6,5 @@ protocol=$3
 CUDA_VISIBLE_DEVICES=0 python action_recognition.py \
   --lr 0.03 \
   --batch-size 512 \
-  --pretrained  ./checkpoints/${dir}/checkpoint_0450
+  --pretrained  ./checkpoints/${dir}/checkpoint_0450.pth.tar \
+  --finetune-dataset ${datas
