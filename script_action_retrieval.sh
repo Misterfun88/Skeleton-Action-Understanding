9512f11272a6cc3c60885405c959499f16ca2c89
Misterfun88 <misterfu7@proton.me>
2024-02-14 13:23:52 +0000

    Updates from review.

--- script_action_retrieval.sh
+++ script_action_retrieval.sh
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES=0 python action_retrieval.py \
   --lr 0.03 \
   --batch-size 512 \
   --knn-neighbours 1 \
-  --pretrained  ./checkpoints/${
+  --pretrained  ./checkpoints/${dir}/checkpoint_0450.pth.t
