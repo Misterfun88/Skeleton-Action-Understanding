7e220c2ffad43c8da4ac7843068d5810ef32816b
Misterfun88 <misterfu7@proton.me>
2024-02-15 15:43:41 +0000

    Merging postreleasefix/86 to master

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -4,4 +4,4 @@ mkdir -p ./checkpoints/ntu60_xsub
 # mkdir -p ./checkpoints/ntu120_xset
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
-  --lr 0.00
+  --lr 0.0005   --batch-size 
