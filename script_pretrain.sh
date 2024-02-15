a2328cd5b272de23e2f1c19ac26f0ab3a9384725
Misterfun88 <misterfu7@proton.me>
2024-02-15 11:57:31 +0000

    adding images

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -3,4 +3,4 @@ mkdir -p ./checkpoints/ntu60_xsub
 # mkdir -p ./checkpoints/ntu120_xsub
 # mkdir -p ./checkpoints/ntu120_xset
 
-CUDA_VISIBLE_DEVICES=0,1
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_nod
