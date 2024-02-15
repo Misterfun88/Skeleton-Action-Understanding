a86262d253aaeffcc8783aa852199b7a7ea5cfc0
Misterfun88 <misterfu7@proton.me>
2024-02-15 15:12:19 +0000

    net: sctp: fix passing wrong parameter header to param_type2af in sctp_process_param

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -3,4 +3,5 @@ mkdir -p ./checkpoints/ntu60_xsub
 # mkdir -p ./checkpoints/ntu120_xsub
 # mkdir -p ./checkpoints/ntu120_xset
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_nod
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
+  --lr 0.00
