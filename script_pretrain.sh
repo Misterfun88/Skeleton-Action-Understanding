8de98ae9f2a154f7af795158afcc4166e7afb982
Misterfun88 <misterfu7@proton.me>
2024-02-16 08:51:22 +0000

    removed obsolete import

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -4,4 +4,5 @@ mkdir -p ./checkpoints/ntu60_xsub
 # mkdir -p ./checkpoints/ntu120_xset
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
-  --lr 0.0005   --batch-size 
+  --lr 0.0005   --batch-size 512  \
+  --checkpoint-
