f19abb5e72a87e0581b75433121cba928fe01ecd
Misterfun88 <misterfu7@proton.me>
2024-02-16 21:31:07 +0000

    notice

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
   --lr 0.0005   --batch-size 512  \
   --checkpoint-path ./checkpoints/ntu60_xsub \
   --schedule 351  --epochs 451  \
-  --pre-dataset 
+  --pre-dataset ntu60  --protocol cross_subject | tee -
