7bd7e671ec9847790ab1ded08b13a41c8091037a
Misterfun88 <misterfu7@proton.me>
2024-02-19 05:58:14 +0000

    2 new ATRs

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -7,4 +7,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
   --lr 0.0005   --batch-size 512  \
   --checkpoint-path ./checkpoints/ntu60_xsub \
   --schedule 351  --epochs 451  \
-  --pre-dataset ntu60  --protocol cross_subject | tee -a ./checkpoints/ntu60_xsub/train.
+  --pre-dataset ntu60  --protocol cross_subject | tee -a ./checkpoints/ntu60_xsub/train.log
+
+# CUDA_VISIBLE_DEVICES=0,1
