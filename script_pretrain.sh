7741659511b77cc870f98fbc4af347cc499eae24
Misterfun88 <misterfu7@proton.me>
2024-02-21 09:54:28 +0000

    Add missing check

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -12,3 +12,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
 # CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
 #   --lr 0.0005   --batch-size 1024  \
 #   --checkpoint-path ./checkpoints/ntu60_xview \
+#   --schedule 351  --e
