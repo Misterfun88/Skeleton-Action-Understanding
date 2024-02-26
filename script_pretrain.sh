31cd7f3ba40cf1302abb9a2bfb3c7bb41e9a00dc
Misterfun88 <misterfu7@proton.me>
2024-02-26 07:24:28 +0000

    Add usage instructions.

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -21,4 +21,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
 #   --schedule 351  --epochs 451  \
 #   --pre-dataset ntu120  --protocol cross_subject | tee -a ./checkpoints/ntu120_xsub/train.log 
 
-# CUDA_VISIBLE_DEVICE
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=
