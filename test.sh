6460f2779333762eda069b418ea20060c7039e89
Misterfun88 <misterfu7@proton.me>
2024-02-29 14:39:05 +0000

    rem

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ if [ ! -d ./checkpoints/${test_name} ];then
 fi
  CUDA_VISIBLE_DEVICES=1,2 torchrun  --nproc_per_node=2 pretrain.py   \
   --lr 0.0005   --batch-size 1024  \
-  --checkpoin
+  --checkpoint-path ./checkpoints/${test_name}_xsu
