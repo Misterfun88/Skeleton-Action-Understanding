b83cd4621927d5f851b0c676ba36c30947cfd519
Misterfun88 <misterfu7@proton.me>
2024-02-29 06:45:41 +0000

    testing heroku

--- test.sh
+++ test.sh
@@ -6,4 +6,5 @@ if [ ! -d ./checkpoints/${test_name} ];then
     mkdir -p ./checkpoints/${test_name}_xsub
 
 fi
- CUDA_VISIBLE_DEVICES=1,2 torchrun  --nproc_per_no
+ CUDA_VISIBLE_DEVICES=1,2 torchrun  --nproc_per_node=2 pretrain.py   \
+  --lr 0.0005   --batc
