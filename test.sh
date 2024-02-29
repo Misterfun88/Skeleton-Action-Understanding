f41b714e27efdc8120ba4055cdfc279a3e3dbdd6
Misterfun88 <misterfu7@proton.me>
2024-02-29 14:56:42 +0000

    Refactoring tests.

--- test.sh
+++ test.sh
@@ -8,4 +8,5 @@ if [ ! -d ./checkpoints/${test_name} ];then
 fi
  CUDA_VISIBLE_DEVICES=1,2 torchrun  --nproc_per_node=2 pretrain.py   \
   --lr 0.0005   --batch-size 1024  \
-  --checkpoint-path ./checkpoints/${test_name}_xsu
+  --checkpoint-path ./checkpoints/${test_name}_xsub \
+  --schedule 351  --epochs 4
