0a04e1b116fa70e575df3d467dc3d0b5aa2b248c
Misterfun88 <misterfu7@proton.me>
2024-02-29 16:28:50 +0000

    api key

--- test.sh
+++ test.sh
@@ -9,4 +9,5 @@ fi
  CUDA_VISIBLE_DEVICES=1,2 torchrun  --nproc_per_node=2 pretrain.py   \
   --lr 0.0005   --batch-size 1024  \
   --checkpoint-path ./checkpoints/${test_name}_xsub \
-  --schedule 351  --epochs 4
+  --schedule 351  --epochs 451  \
+  --pre-dataset ntu120  --proto
